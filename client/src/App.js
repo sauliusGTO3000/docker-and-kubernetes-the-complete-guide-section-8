a97aa8efd2ec930fe7bad0b6a0edce575585c46d
sauliusGTO3000 <sauliusgto3000@gmail.com>
2022-06-02 19:31:38 +0300

    react app routing and pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, route, Link } from 'react-router-dom';
+import OtherPage from "./OtherPage";
+import fib from "./Fib";
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <Link to="/">Home</Link>
+            <Link to="/otherPage">Other Page</Link>
+          </header>
+          <div>
+            <Router exactPath = '/' component={'Fib'} />
+            <Router exactPath = '/otherPage' component={'OtherPage'} />
+          </div>
+        </div>
+      </Router>
   );
 }
 
---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+    return (
+        <div>
+            This is another page!
+            <link to="/"> back to homepage </link>
+        </div>
+    );
+};
